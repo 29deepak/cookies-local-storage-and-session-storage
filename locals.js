fc4e3ef52b25b7c900829503f2fa870f6b6979d0
deepak <deepak545608@gmail.com>
2022-11-06 21:44:06 +0530

    local comm

---
+++ locals.js
@@ -0,0 +1,9 @@
+localStorage.setItem('name', 'Kyle')
+
+sessionStorage.setItem('name', 'Kyle')
+document.cookie = 'name=Kyle'
+localStorage.setItem('name', 'Kyle')
+localStorage.setItem('lastName', 'Smith')
+
+sessionStorage.setItem('name', 'Kyle')
+sessionStorage.setItem('lastName', 'Smith')
